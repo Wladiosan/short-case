25d8c44ae2f4a8d500cdcf326867cd708963e3d4
Wladiosan <w.ohrimenko@gmail.com>
2021-07-30 23:06:27 +0300

    update entry point in app

--- Express-PostgreSQL/app.js
+++ Express-PostgreSQL/app.js
@@ -10,4 +10,4 @@ app.use(express.json())
 app.use('/api', userRouter)
 app.use('/api', postRouter)
 
-app.listen(PORT, () => console.log(`Server was running at ${PORT} port...`))
+app.listen(PORT, () => console.log(`Server is running at ${PORT} port...`))
